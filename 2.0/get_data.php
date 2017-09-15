6b7659a300c0e60bb24c309a920ca88bd72cc7c3
Null <abc612008@126.com>
2017-09-15 20:34:19 +0800

    check if username is empty

--- 2.0/get_data.php
+++ 2.0/get_data.php
@@ -1,14 +1,17 @@
 <?php
 require_once 'vendor/autoload.php'; // composer autoloader
 
+if(!isset($_POST["username"]))
+    exit("No username is given");
+
 $username = preg_replace('/[^\w]+/','',$_POST["username"]);
 $password = preg_replace('/[^\w]+/','',$_POST["password"]);
 try {
     $student = PowerAPI\PowerAPI::authenticate("http://powerschool.mapleleaf.cn", $username,$password);
 } catch (PowerAPI\Exceptions\Authentication $e) {
-    file_put_contents("error.log.py", date('Y-m-d H:i:s') . ' ' .  $e->getMessage() . "\n",FILE_APPEND);
-    die('Something went wrong! '.$e->getMessage());
+    file_put_contents("../error.log.py", date('Y-m-d H:i:s') . ' ' .  $e->getMessage() . "\n",FILE_APPEND);
+    exit('Something went wrong! '.$e->getMessage());
 }
-file_put_contents("usage.log.py", '2.0 ' . date('Y-m-d H:i:s') . ' ' .  $username . "\n",FILE_APPEND);
+file_put_contents("../usage.log.py", '2.0 ' . date('Y-m-d H:i:s') . ' ' .  $username . "\n",FILE_APPEND);
 
 echo json_encode($student);
