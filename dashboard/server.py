9aa25a1d59ef5bdd50d74a8958154aab3eed9a1b
Null <abc612008@126.com>
2017-11-11 17:24:32 +0800

    better dashborad

--- dashboard/server.py
+++ dashboard/server.py
@@ -4,53 +4,61 @@ from flask import Flask, render_template
 from datatoaster import *
 import collections
 import datetime
+import time
 
 data = collections.OrderedDict()
 
 
 class Item:
-    def __init__(self, api_version, date, username, action, version, os):
-        self.api_version, self.date, self.username, self.action, self.version, self.os = api_version, date, username, action, version, os
+    def __init__(self, api_version, date, time, username, action, version, os):
+        self.api_version, self.date, self.time, self.username, self.action, self.version, self.os = api_version, date, time, username, action, version, os
 
     def __repr__(self):
         return repr(self.__dict__)
 
 
-all_data = []
-
 # import data
-with open("/var/www/html/api/usage.log.py") as file:
-    for line in file:
-        parts = line.split(" ")
-        if len(parts) == 3:  # old version: 2017-09-24 10:00:00 'xxxxxxxx'
-            api_version = "1.0"
-            date = parts[0]
-            username = parts[2].replace("'", "").replace("\n", "")
-            action = "unknown"
-            version = "1.0.x"
-            os = "unknown"
-        elif len(parts) == 4:  # updated api: 1.0 2017-09-24 10:00:00 'xxxxxxxx'
-            api_version = parts[0]
-            date = parts[1]
-            username = parts[3].replace("'", "").replace("\n", "")
-            action = "unknown"
-            version = "1.1_beta"
-            os = "unknown"
-        elif len(parts) == 7:  # latest api: 2.0 2017-09-24 18:01:49 xxxxxxxx pull_data 1.1 android
-            api_version = parts[0]
-            date = parts[1]
-            username = parts[3]
-            version = parts[4]
-            action = parts[5]
-            os = parts[6].replace("\n", "")
-        else:
-            print("[Warning] Skipped a line: ", line)
-            continue
-        all_data.append(Item(api_version, date, username, action, version, os))
+def import_data(file_name):
+    all_data = []
+    with open(file_name) as file:
+        for line in file:
+            parts = line.split(" ")
+            if len(parts) == 3:  # old version: 2017-09-24 10:00:00 'xxxxxxxx'
+                api_version = "1.0"
+                date = parts[0]
+                time = parts[1]
+                username = parts[2].replace("'", "").replace("\n", "")
+                action = "unknown"
+                version = "1.0.x"
+                os = "unknown"
+            elif len(parts) == 4:  # updated api: 1.0 2017-09-24 10:00:00 'xxxxxxxx'
+                api_version = parts[0]
+                date = parts[1]
+                time = parts[2]
+                username = parts[3].replace("'", "").replace("\n", "")
+                action = "unknown"
+                version = "1.1_beta"
+                os = "unknown"
+            elif len(parts) == 7:  # latest api: 2.0 2017-09-24 18:01:49 xxxxxxxx pull_data 1.1 android
+                api_version = parts[0]
+                date = parts[1]
+                time = parts[2]
+                username = parts[3]
+                version = parts[4]
+                action = parts[5]
+                os = parts[6].replace("\n", "")
+            else:
+                print("[Warning] Skipped a line: ", line)
+                continue
+            all_data.append(Item(api_version, date, time, username, action, version, os))
+    return all_data
+
 
 app = Flask(__name__)
 
-Width, Height, Curve = 700, 500, 0.1
+Width, Height, Curve = 700, 500, 0.5
+
+all_data = import_data("../usage.log.py")
 
 
 def standardized(data):
@@ -63,11 +71,26 @@ def standardized(data):
         for k in all_keys:
             arr.append(value.get(k, 0))
         data[key] = arr
-    return data, all_keys
+    return data, list(all_keys)
+
+
+def get_peak(data):
+    peak = 0
+    peak_date = "n/a"
+    for date, num in data.items():
+        if num > peak:
+            peak = num
+            peak_date = date
+    return peak, peak_date
+
+
+line_chart_args = {"is_label_show": True, "is_datazoom_show": True, "line_curve": Curve, "is_smooth": True, "is_fill":True, "area_opacity":0.3}
 
 
 @app.route("/dashboard/")
 def index():
+    time_start = time.time()
+
     today_date = datetime.datetime.now().strftime('%Y-%m-%d')
     yesterday_date = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
 
@@ -85,8 +108,7 @@ def index():
                          .ordered_by(lambda i: i[0])
                          .get_result())
     number_chart = Line("User Numbers", width=Width, height=Height)
-    number_chart.add("", list(number_chart_data.keys()), list(number_chart_data.values()), is_label_show=True,
-                     is_datazoom_show=True, line_curve=Curve)
+    number_chart.add("", list(number_chart_data.keys()), list(number_chart_data.values()), **line_chart_args)
 
     number_usage_chart_data = (datatoaster.DataSet(all_data)
                                .set_x(lambda i: i.date)
@@ -95,12 +117,38 @@ def index():
                                .get_result())
     number_usage_chart = Line("User Usage Numbers", width=Width, height=Height)
     number_usage_chart.add("", list(number_usage_chart_data.keys()), list(number_usage_chart_data.values()),
-                           is_label_show=True, is_datazoom_show=True, line_curve=Curve)
+                           **line_chart_args)
 
     today_user, today_usage = number_chart_data.get(today_date, 0), number_usage_chart_data.get(today_date, 0)
-    diff_user, diff_usage = today_user - number_chart_data.get(yesterday_date,
-                                                               0), today_usage - number_usage_chart_data.get(
-        yesterday_date, 0)
+    diff_user, diff_usage = today_user - number_chart_data.get(yesterday_date, 0), \
+                            today_usage - number_usage_chart_data.get(yesterday_date, 0)
+
+    nu_set = datatoaster.DataSet(all_data)
+    number_usage_today_chart_data, nu_series = standardized(nu_set
+                                                            .set_x(lambda i: i.time[:5])
+                                                            .set_y(nu_set.NumberOfAppearance(lambda i: i.action))
+                                                            .add_constraint(lambda i: i.date == today_date)
+                                                            .ordered_by(lambda i: i[0])
+                                                            .get_result())
+    number_usage_today_chart = Line("User Usage\nNumbers (Today) By Requests", width=Width, height=Height)
+    for i in range(0, len(next(iter(number_usage_today_chart_data.values())))):
+        number_usage_today_chart.add(nu_series[i], list(number_usage_today_chart_data.keys()),
+                                     [j[i] for j in list(number_usage_today_chart_data.values())], **line_chart_args,
+                                     is_stack=True)
+
+    user_peak, user_peak_date = get_peak(number_chart_data)
+    usage_peak, usage_peak_date = get_peak(number_usage_chart_data)
+
+    os_time_chart_data_set = datatoaster.DataSet(unique_data_array)
+    os_time_chart_data_set_data, os_time_series = standardized(os_time_chart_data_set
+                                                       .set_x(lambda i: i.date)
+                                                       .set_y(os_time_chart_data_set.NumberOfAppearance(lambda i: i.os))
+                                                       .ordered_by(lambda i: i[0])
+                                                       .get_result())
+    os_time_chart = Line("Versions", width=Width, height=Height)
+    for i in range(0, len(next(iter(os_time_chart_data_set_data.values())))):
+        os_time_chart.add(os_time_series[i], list(os_time_chart_data_set_data.keys()),
+                          [j[i] for j in list(os_time_chart_data_set_data.values())], **line_chart_args, is_stack=True)
 
     os_chart_data_set = datatoaster.DataSet(unique_data_array)
     os_chart_data = (os_chart_data_set
@@ -131,20 +179,6 @@ def index():
     version_android_chart.add("", list(version_android_chart_data.keys()), list(version_android_chart_data.values()),
                               is_label_show=True)
 
-    version_chart_data_set = datatoaster.DataSet(unique_data_array)
-    version_chart_data_set_data, series = standardized(version_chart_data_set
-                                                       .set_x(lambda i: i.date)
-                                                       .set_y(
-        version_chart_data_set.NumberOfAppearance(lambda i: "%s (%s)" % (i.version, i.os)))
-                                                       .ordered_by(lambda i: i[0])
-                                                       .get_result())
-    series = list(series)
-    version_chart = Line("Versions", width=Width, height=Height)
-    for i in range(0, len(next(iter(version_chart_data_set_data.values())))):
-        version_chart.add(series[i], list(version_chart_data_set_data.keys()),
-                          [j[i] for j in list(version_chart_data_set_data.values())], is_label_show=True,
-                          is_datazoom_show=True, line_curve=Curve)
-
     action_chart_data_set = datatoaster.DataSet(all_data)
     action_chart_data = (action_chart_data_set
                          .set_x(lambda i: i.action)
@@ -156,24 +190,21 @@ def index():
 
     district_chart_data_set = datatoaster.DataSet(unique_data_array)
     district_chart_data = (district_chart_data_set
-                           .set_x(lambda i: i.username[2:4] if i.username[:4]!="2017" else i.username[4:6])
+                           .set_x(lambda i: i.username[2:4] if i.username[:4] != "2017" else i.username[4:6])
                            .set_y(district_chart_data_set.NumberOfAppearance(datatoaster.XValue))
-                           .add_constraint(
-        lambda i: (i.username[2:4] if i.username[:4]!="2017" else i.username[4:6]).isdigit())
+                           .add_constraint(lambda i: i.username[:8].isdigit())
                            .get_result())
     district_chart = Pie("Percentage of\nDistrict Code", width=Width, height=Height)
     district_chart.add("", list(district_chart_data.keys()), list(district_chart_data.values()), is_label_show=True)
 
     grade_chart_data_set = datatoaster.DataSet(unique_data_array)
     grade_chart_data = (grade_chart_data_set
-                           .set_x(lambda i: i.username[0:2] if i.username[:4]!="2017" else i.username[0:4])
-                           .set_y(grade_chart_data_set.NumberOfAppearance(datatoaster.XValue))
-                           .add_constraint(
-        lambda i: (i.username[0:2] if i.username[:4]!="2017" else i.username[0:4]).isdigit())
-                           .get_result())
+                        .set_x(lambda i: i.username[0:2] if i.username[:4] != "2017" else i.username[0:4])
+                        .set_y(grade_chart_data_set.NumberOfAppearance(datatoaster.XValue))
+                        .add_constraint(lambda i: i.username[:8].isdigit())
+                        .get_result())
     grade_chart = Pie("Percentage of\nGrades", width=Width, height=Height)
     grade_chart.add("", list(grade_chart_data.keys()), list(grade_chart_data.values()), is_label_show=True)
-
 
     return render_template('index.html',
                            os_chart=os_chart.render_embed(),
@@ -181,10 +212,18 @@ def index():
                            version_android_chart=version_android_chart.render_embed(),
                            version_ios_chart=version_ios_chart.render_embed(),
                            number_usage_chart=number_usage_chart.render_embed(),
+                           number_today_chart=number_usage_today_chart.render_embed(),
                            action_chart=action_chart.render_embed(),
                            district_chart=district_chart.render_embed(),
                            grade_chart=grade_chart.render_embed(),
-                           version_chart=version_chart.render_embed(),
+                           os_time_chart=os_time_chart.render_embed(),
+                           user_peak=user_peak,
+                           user_peak_date=user_peak_date,
+                           usage_peak=usage_peak,
+                           usage_peak_date=usage_peak_date,
+                           time_used=round((time.time() - time_start) * 1000, 2),
+                           total_users=len(set([i.username for i in all_data])),
+                           total_requests=len(all_data),
                            today_user=today_user,
                            today_usage=today_usage,
                            diff_user=("+" if diff_user > 0 else "") + str(diff_user),
