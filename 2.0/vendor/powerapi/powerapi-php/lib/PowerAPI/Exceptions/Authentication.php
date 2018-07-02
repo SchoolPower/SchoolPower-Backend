5b5986c389cd19b86dd41e53481008fce2a5c157
Harry Null <harryyunull@gmail.com>
2018-07-02 19:01:24 +0800

    [IMPROVEMENT] Remove dependencies -- these should be obtained via composer

--- 2.0/vendor/powerapi/powerapi-php/lib/PowerAPI/Exceptions/Authentication.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace PowerAPI\Exceptions;
-
-/** Exception class to specifically handle Authentication related exceptions. */
-class Authentication extends Exception { }
--- 2.0/vendor/powerapi/powerapi-php/lib/PowerAPI/Exceptions/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace PowerAPI\Exceptions;
-
-/** Exception class to allow users to exclusively catch PowerAPI exceptions */
-class Exception extends \Exception { }
