7d7ea93742c734e26294c8a5d5abd0d82c1fe0ad
Harry Null <harryyunull@gmail.com>
2018-07-29 11:08:51 +0800

    [Fix] Better error message

--- 2.0/get_data.php
+++ 2.0/get_data.php
@@ -29,7 +29,7 @@ try {
 } catch (PowerAPI\Exceptions\Authentication $e) {
     $statsd->endTiming("fetch_data_time");
     $statsd->increment("failed_call");
-    exit('{"err":"200","description":"'.addslashes($e->getMessage()).'","reserved":"Something went wrong! Invalid Username or password"}');
+    exit('{"err":"200","description":"'.addslashes($e->getMessage()).'","reserved":"Something went wrong! '.addslashes($e->getMessage()).'"}');
 }
 
 // Get avatar from database; comment them out if you don't need it
